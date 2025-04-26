fa42869417a0e934c1d893fa274a6b0a204fc009
= <=>
2025-04-26 14:32:50 +0200

    Dodalem interface-y

--- components/city/action.ts
+++ components/city/action.ts
@@ -1,11 +1,15 @@
 import {City} from "@/types/City";
 
 export interface CitiesControllerInterface {
-
+    id: string;
+    name: string;
+    status: boolean;
+    teachersNo: number;
+    studentsNo: number;
 }
 
 //CRUD
-export function fetchCities() : Promise<CitiesControllerInterface>
+export function fetchCity(id:string) : Promise<CitiesControllerInterface>
 {
 
 }
