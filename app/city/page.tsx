911f9056bf90d3e6d0a893dab19c544412aae2d0
mi-reniewicz <mi.reniewicz@u.zs3-wyszkow.pl>
2025-04-26 13:36:39 +0200

    Dodanie objektu - (cities)

--- app/city/page.tsx
+++ app/city/page.tsx
@@ -9,7 +9,32 @@ import {
 } from "@/components/ui/table"
 
 export default function CityPage() {
-
+    let cities = [
+        {
+            miasto: "Łódź",
+            status: "🔴",
+            liczbaNauczycieli: 18,
+            liczbaUczniow: 68
+        },
+        {
+            miasto: "Poznań",
+            status: "🔴",
+            liczbaNauczycieli: 16,
+            liczbaUczniow: 52
+        },
+        {
+            miasto: "Wyszków",
+            status: "🟢",
+            liczbaNauczycieli: 5,
+            liczbaUczniow: 29
+        },
+        {
+            miasto: "Warszawa",
+            status: "🟢",
+            liczbaNauczycieli: 23,
+            liczbaUczniow: 71.5
+        }
+]
 
     return (
         <>
